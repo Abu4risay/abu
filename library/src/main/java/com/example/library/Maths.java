3b4bb2da9a1ecef467c9491844cc945f2a11d9e5
Abu4risay <abu4risay1@gmail.com>
2021-07-22 08:43:05 +0100

    a

---
+++ library/src/main/java/com/example/library/Maths.java
@@ -0,0 +1,21 @@
+package com.example.library;
+
+public class Maths {
+    
+    public static int Plus(int a, int b)
+    {
+		return a+b;
+	}
+	
+	public static final int Minus(int a, int b)
+    {
+		return a-b;
+	}
+	
+	public static int Times(int a, int b)
+    {
+		return a*b;
+	}
+	
+	
+}
